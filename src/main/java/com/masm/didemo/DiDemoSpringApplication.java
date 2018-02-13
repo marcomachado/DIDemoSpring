3c27c2d6916a8d078306c8578a33cd68bdde5005
Marco André Machado <marco.santos.machado@gmail.com>
2018-02-13 18:50:17 -0300

    First commit

---
+++ src/main/java/com/masm/didemo/DiDemoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.masm.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/masm/didemo/DiDemoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.masm.didemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiDemoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
